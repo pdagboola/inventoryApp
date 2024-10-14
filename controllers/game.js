1c7d56ee5e0205f934d4cafefdfdf8fe67624d22
Peter Agboola <pdagboola@gmail.com>
2024-10-14 12:20:18 -0700

    uunfixedBugNowFixed

--- controllers/game.js
+++ controllers/game.js
@@ -10,15 +10,16 @@ game.get("/", async (req, res) => {
   res.render("game", { games });
 });
 
-game.get("/:title", async (req, res) => {
+game.get("/:title/:id", async (req, res) => {
   // const array = req.params;
   // console.log("Here's the array retrieved from parameters", array);
   const { title, id } = req.params;
+  console.log({ title, id });
   console.log("Here's the id retrieved from the params", id);
   const games = await getGame(title);
-  // const genreName = await gameGenre(id);
-  // console.log("Here's the name of the game genre", genreName);
-  res.render("viewGame", { games: games });
+  const genreName = await gameGenre(id);
+  console.log("Here's the name of the game genre", genreName);
+  res.render("viewGame", { games: games, genres: genreName });
 });
 
 module.exports = game;
